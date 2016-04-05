06317972f58917334e71eac6fff24e842827ee27
Genevieve1722 <gsublette@gmail.com>
2016-04-05 16:21:15 -0700

    Final version

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 angular.module('app', [])
 
-// myApp.controller('IdeasController', ['$scope', '$data', function($scope, $data) {
-//   console.log($data);
-//   console.log('data');
-//   $scope.technologies = $data.technologies;
-//   $scope.audiences = $data.audiences;
-
-//   $scope.generateIdea = function() {
-//     console.log('generate idea called');
-//     var tech = $scope.technologies[Math.floor(Math.random()*$scope.technologies.length)];
-//     var audiences = $scope.audiences[Math.floor(Math.random()*$scope.audiences.length)];
-//     return tech + ', but for ' + audiences;
-//   }
-
-
-// }]);
-
-
 .controller('IdeasController', function($scope) {
 
   console.log('app.js was loaded');
--- data.js
+++ data.js
@@ -1,8 +1,8 @@
-var technologies = ['Tinder', 'Google Maps', 'Waze', 'Facebook', 
+var technologies = ['Tinder', 'Avalon', 'Google Maps', 'Waze', 'Facebook', 
 'Slack', 'Hulu', 'Netflix', 'Instagram', 'Pinterest', 'Uber', 'Spotify', 'YouTube',
 'WhatsApp', 'Angry Birds', 'Skype', 'Temple Run', 'Dropbox', 'Candy Crush', 'Bitcoin',
-'LinkedIn', 'Github', 'Stack Overflow']
+'LinkedIn', 'Github', 'Stack Overflow', 'Strava', 'Square', 'eBay', 'Shazam']
 
 var audiences = ['missing socks', 'grandparents', 'kids', 'cats', 'dogs', 
 'grey animals', '6th floor', 'Tenderloin residents', 'Dodgers fans', 'east coasters',
-'friends', 'enemies', 'rednecks']
+'friends', 'enemies', 'rednecks', 'bros']
